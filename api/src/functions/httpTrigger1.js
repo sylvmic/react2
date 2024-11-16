fd6277bcaf900dded0bdd956f8602f7c82319a56
« Sylvain <sylvain.michel7@gmail.com>
2024-11-16 05:27:01 -0500

    test

--- api/src/functions/httpTrigger1.js
+++ api/src/functions/httpTrigger1.js
@@ -3,10 +3,10 @@ const { app } = require('@azure/functions');
 app.http('httpTrigger1', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: async (request, context) => {
+    handler: (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
+        const name = 'world678678';
 
         return { body: `Hello, ${name}!` };
     }
--- testreact/src/App.js
+++ testreact/src/App.js
@@ -6,18 +6,20 @@ function App() {
   const [data, setData] = useState('');
 
   useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/httpTrigger1`)).json();
-      setData(text);
-    })();
-  });
+    fetch('/api/httpTrigger1')
+      .then(response => response.text())
+      .then(text => setData(text))
+      .catch(error => console.error(error));
+  }, []);
+
+  const test = 'banane'
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. ghkghj {data} {test}
         </p>
         <a
           className="App-link"
@@ -25,8 +27,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React {data}
+          Learn React
         </a>
+        {data}
       </header>
     </div>
   );
